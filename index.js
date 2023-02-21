36cc0af1a7526c1f38719d7f87e23d88a33df072
Rozmuhammad2022 <otushbekbaxtiyorov97@gmail.com>
2023-02-21 17:05:40 +0500

    full Path to views created

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ import express from 'express';
 import layout from 'express-ejs-layouts';
 
 import db from './src/config/db.config.js';
-import router from './src/router/home.router.js'
+import router from './src/router/home.router.js';
+
+import {fileURLToPath} from 'url';
+import {dirname, join} from 'path';
+
 const app = express();
 db();
+
+const Full_Path = dirname(fileURLToPath(import.meta.url));
+
 app.set('view engine', 'ejs');
+app.set('views', join(Full_Path, 'src', 'views'));
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public")); 
