87a99cda3fb00dba961fbd59c346fceee0081f17
Rozmuhammad2022 <otushbekbaxtiyorov97@gmail.com>
2023-02-21 16:52:43 +0500

    full path to views created

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ import mongoose from 'mongoose';
 import session from 'express-session';
 import layout from 'express-ejs-layouts';
 import Middlewares from './src/middlewares/middle.js';
-
+import {fileURLToPath} from 'url';
+import { dirname, join } from 'path';
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://localhost/it-center', {useNewUrlParser: true, useUnifiedTopology: true});
 const app = express();
 
 Middlewares(app);
 
+const Full_Path = dirname(fileURLToPath(import.meta.url));
+
+
 app.set('view engine', 'ejs');
-app.set('views', './src/views/');
+app.set('views', join(Full_Path, 'src', 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public")); 
 app.use(express.json());
