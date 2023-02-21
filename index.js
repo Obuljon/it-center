45b0bd0fc129753a14eed3a6e38acc722ca85213
rayimberdiyevhojiakbar08 <rayimberdiyevhojiakbar08@gmail.com>
2023-02-21 16:58:47 +0500

    home

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import layout from 'express-ejs-layouts';
-mongoose.set('strictQuery', false)
-mongoose.connect('mongodb://localhost/it-center', {useNewUrlParser: true, useUnifiedTopology: true});
+import db from './src/config/db.config.js';
+import router from './src/router/home.router.js'
 const app = express();
-
+db();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public")); 
